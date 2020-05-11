1399216f5b52dbab934404d60b164cfadd927cc2
xmy0916 <863299715@qq.com>
2020-05-11 12:10:04 +0800

    修改py版本巡线代码hsv参数

--- racecar_gazebo/scripts/findLine.py
+++ racecar_gazebo/scripts/findLine.py
@@ -19,7 +19,7 @@ class Follower:
   def image_callback(self, msg):
     image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_yellow = numpy.array([ 0,  0, 221])
+    lower_yellow = numpy.array([ 0,  0, 100])
     upper_yellow = numpy.array([180, 30, 255])
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
     
